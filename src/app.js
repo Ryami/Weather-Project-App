65c0aa88333c0da2bd94060e987b6077ba9f0e0c
Ryami <michelle.cabasag5@gmail.com>
2022-09-26 21:28:12 -0700

    response to get day and coords

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -51,7 +51,8 @@ function displayForecast() {
 function getforecast(coordinates) {
   console.log(coordinates);
   let apiKey = "fa5cccda27fd9109e12bd86078c8d2e8";
-  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${aipKey}`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${aipKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -118,4 +119,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Location");
-displayForecast();
